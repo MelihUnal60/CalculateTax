31780a6a29dadaeb64ba71ba322cd266b0d10340
MelihUnal60 <111892158+MelihUnal60@users.noreply.github.com>
2023-01-14 08:03:23 +0300

    Add project files.

---
+++ CalculateTax/CalculateTaxPayment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateTax
+{
+    public class CalculateTaxPayment
+    {
+        
+        public static Tax1 Tax(decimal total,CalculateTaxOptions options)
+        {
+
+            Tax1 tax1 = new Tax1();
+            tax1.KDV = total * options.KDVRate;
+            var totalWithoutKDV = total - tax1.KDV;
+            
+            if(options.IsCo)
+            {
+                tax1.tax2 = totalWithoutKDV * options.corporationTax;
+            }
+            else
+            {
+                tax1.tax2 = totalWithoutKDV * options.incomeTax;
+            }
+            return tax1;
+        }
+
+    }
+    public class CalculateTaxOptions
+    {
+        public decimal KDVRate = .18m;
+        public decimal incomeTax = .17m;
+        public decimal corporationTax = .23m;
+        public bool IsCo = false;
+    }
+
+    public class Tax1
+    {
+        public decimal KDV;
+        public decimal tax2;
+    }
+}
---
+++ CalculateTax/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using CalculateTax;
+
+Console.WriteLine("Matrah girin");
+decimal total = Convert.ToDecimal(Console.ReadLine());
+
+CalculateTaxOptions options = new CalculateTaxOptions();
+options.IsCo = false;
+options.KDVRate = .08m;
+
+Tax1 tax1 = CalculateTaxPayment.Tax(total,options);
+
+Console.WriteLine($"KDV : {tax1.KDV}");
+Console.WriteLine($"Vergi : {tax1.tax2}");
